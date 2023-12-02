0db5cb04fa5ec1e1879158d349adb73b951cee4f
ashlesss <hidahza@gmail.com>
2023-12-01 22:23:33 -0500

    fixing highlighting

---
+++ src/assets/out-codeBlocks.md
@@ -0,0 +1,85 @@
+Certainly! Here are some dummy code blocks for various programming languages:
+
+Python:
+```python
+# Example 1
+def greet(name):
+    print("Hello, " + name + "!")
+
+# Example 2
+def calculate_area(length, width):
+    return length * width
+
+# Example 3
+def find_max(lst):
+    max_val = lst[0]
+    for num in lst:
+        if num > max_val:
+            max_val = num
+    return max_val
+```
+
+JavaScript:
+```javascript
+// Example 1
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+
+// Example 2
+function calculateArea(length, width) {
+    return length * width;
+}
+
+// Example 3
+function findMax(lst) {
+    let maxVal = lst[0];
+    for (let num of lst) {
+        if (num > maxVal) {
+            maxVal = num;
+        }
+    }
+    return maxVal;
+}
+```
+
+Java:
+```java
+// Example 1
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name = "John";
+        System.out.println("Hello, " + name + "!");
+    }
+}
+
+// Example 2
+public class Rectangle {
+    private int length;
+    private int width;
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public int calculateArea() {
+        return length * width;
+    }
+}
+
+// Example 3
+public class ArrayUtils {
+    public static int findMax(int[] arr) {
+        int maxVal = arr[0];
+        for (int num : arr) {
+            if (num > maxVal) {
+                maxVal = num;
+            }
+        }
+        return maxVal;
+    }
+}
+```
+
+Please note that these code blocks are meaningless and are only provided as dummy examples.
--- src/pages/ResultPage.vue
+++ src/pages/ResultPage.vue
@@ -1,40 +1,77 @@
 <template>
-    <div v-html="renderedMarkdown"></div>
+    <div v-html="renderMarkdown()"></div>
 </template>
+  
+
 <script>
 import MarkdownIt from 'markdown-it';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/default.css';
+import { h } from 'vue';
 
 export default {
   data() {
     return {
-      md: new MarkdownIt(),
-      markdownContent: ""
+      md: null,
+      mdContent: ''
     };
+  },
+  created() {
+    this.md = new MarkdownIt({
+      highlight: function (str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+          try {
+            return hljs.highlight(str, { language: lang }).value;
+          } catch (__) {}
+        }
+        return ''; // use external default escaping
+      }
+    });
+
+    // Custom renderer for code blocks
+    const defaultRender = this.md.renderer.rules.fence || function(tokens, idx, options, env, self) {
+      return self.renderToken(tokens, idx, options);
+    };
+
+    this.md.renderer.rules.fence = function(tokens, idx, options, env, self) {
+        const token = tokens[idx];
+        const langName = token.info.trim();
+        if (hljs.getLanguage(langName)) {
+            return `<pre class="my-code-block"><div class="language-label">${langName}</div>${defaultRender(tokens, idx, options, env, self)}</pre>`;
+        }
+        else {
+            return `<pre class="my-code-block">${defaultRender(tokens, idx, options, env, self)}</pre>`;
+        }
+      
+    };
+  },
+  methods: {
+    renderMarkdown() {
+      return this.md.render(this.mdContent);
+    }
   },
 
   mounted() {
-    this.requestMarkdownFile()
-    // console.log(this.markdownContent)
-  },
-
-  methods: {
-    requestMarkdownFile() {
-        // this.$axios.get('https://raw.githubusercontent.com/ashlesss/querySys/main/update_log.md')
-        // .then(res => {
-        //     console.log(res)
-        //     this.markdownContent = res.data
-        // })
-    }
-  },
-
-  computed: {
-    renderedMarkdown() {
-      return this.md.render(this.markdownContent);
-    }
+    this.$axios.get('https://raw.githubusercontent.com/ashlesss/cheatBot-frontend/main/src/assets/out.md')
+        .then(res => {
+            this.mdContent = res.data
+        })
   }
 };
 </script>
+<style>
+.my-code-block {
+  background-color: #f5f5f5;
+  border: 1px solid #ddd;
+  padding: 10px;
+  border-radius: 4px;
+  overflow: auto;
+}
 
-<style lang="">
-    
+.language-label {
+    background-color: #000000;
+    color: #ffffff;
+    font-weight: bold;
+    font-size: 24px;
+}
 </style>
