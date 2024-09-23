51b6385a249f3914359e006d1e298e9381ed2b27
hashlesss <hidahza@gmail.com>
2024-09-23 19:13:31 -0400

    Patch markdown-it-texmath to render inline math equation correctly

---
+++ build.sh
@@ -0,0 +1,19 @@
+
+if quasar build; then 
+  echo "* Quasar build successful"
+  if [ -d "/var/www/cheatBot/" ]; then
+    rm -r -f /var/www/cheatBot/dist/
+    echo "* Old dist directory removed successfully"
+  else
+    echo "* Directory /var/www/cheatBot/ does not exist"
+    echo "* Creating /var/www/cheatBot/"
+    mkdir -p /var/www/cheatBot/
+  fi
+
+  mv ./dist /var/www/cheatBot/ && echo "* DONE" || echo "Failed"
+else
+  echo "Quasar build failed"
+fi
+
+
+
